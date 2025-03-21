bead11c16488998b819696b5bbb2b9043a7c5d79
jinys0527 <jinys0527@naver.com>
2025-03-21 18:46:46 +0900

    파일 이름 수정 및 Render 구현

---
+++ RhythmHeaven/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <Windows.h>
+
+//int main()
+//{
+//	/*StartGame();
+//	while (IsGameRun())
+//	{
+//		ProcessInput();
+//		Update();
+//		Render();
+//	}
+//	EndGame();*/
+//
+//	return 0;
+//}
